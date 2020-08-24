a6db8f72b3810fb0ca2a9053c60b7e17865ce225
Bnaya Peretz <bnaya@testim.io>
2020-08-24 16:49:44 +0300

    Fix node 10 stacktrace issue (#1)

--- packages/root-cause-core/lib/consts.ts
+++ packages/root-cause-core/lib/consts.ts
@@ -6,3 +6,7 @@ export const RUN_CONCLUSION_FILE_NAME = 'runConclusion.json';
 export const FALLBACK_RUN_ID = 'no_run_id';
 export const HISTORY_FILE_NAME = 'history.json';
 export const HISTORY_RUNS_TO_RETAIN = 5;
+
+export const NODE_VERSION = parseFloat(process.versions.node);
+
+export const IS_NODE_10 = NODE_VERSION < 12 && NODE_VERSION > 9;
