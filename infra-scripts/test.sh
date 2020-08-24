da001debb3d3ca96427de25b7988509ad9d9c5f6
Bnaya Peretz <bnaya@testim.io>
2020-08-24 17:50:13 +0300

    Don't run codelessTestRun for now

--- infra-scripts/test.sh
+++ infra-scripts/test.sh
@@ -4,4 +4,4 @@ yarn workspace @testim/root-cause-core test
 yarn workspace @testim/root-cause-mocha test
 yarn workspace @testim/root-cause-jest test
 yarn workspace @testim/root-cause-jest test:integration
-yarn ts-node infra-scripts/codelessTestRun.ts
+# yarn ts-node infra-scripts/codelessTestRun.ts
