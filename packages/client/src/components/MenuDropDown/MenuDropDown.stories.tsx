290d8c30d682d602c8815d923a7f2e8df8ce22c1
Bnaya Peretz <bnaya@testim.io>
2020-08-25 14:46:38 +0300

    Network logs UI iteration

---
+++ packages/client/src/components/MenuDropDown/MenuDropDown.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MenuDropDown } from './MenuDropDown';
+import { action } from '@storybook/addon-actions';
+
+import HarIcon from '../../globalAssets/har@3x.svg';
+import TestLogIcon from '../../globalAssets/TestLog.svg';
+
+export default {
+    title: 'Menu drop down',
+    component: MenuDropDown,
+};
+
+export function AsIs() {
+    return (
+        <MenuDropDown
+            onItemClick={action('item-click')}
+            items={[
+                {
+                    slug: 'download_har_file',
+                    text: 'Download HAR file',
+                    icon: HarIcon,
+                },
+                {
+                    slug: 'download_log_file',
+                    text: 'Download test log',
+                    icon: TestLogIcon,
+                },
+            ]}
+        />
+    );
+}
--- packages/client/src/components/StepResultTitlebar/StepResultTitlebar.tsx
+++ packages/client/src/components/StepResultTitlebar/StepResultTitlebar.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import type { StepResult } from "@testim/root-cause-types";
-import { useState, useRef, useLayoutEffect, useMemo, useEffect } from "react";
+import { useState, useRef, useLayoutEffect } from "react";
 import styles from './styles.module.css';
 import classnames from "classnames";
 import type { Har } from "har-format";
-import downloadHarButton from "./../../globalAssets/archive.svg";
-import { harInTimeRange } from "../../utils/harEntriesInTimeRange";
 
 export const StepResultTitlebar = function StepResultTitlebar({
     selectedStep,
@@ -23,33 +21,6 @@ export const StepResultTitlebar = function StepResultTitlebar({
     const [ hover, setHover ]  = useState<boolean>(false);
     const [ toolTipNeeded, setToolTipNeeded ]  = useState<boolean>(false);
     const stepNameRef = useRef<HTMLSpanElement>(null);
-    const [harFileObjectUrl, setHarFileObjectUrl] = React.useState<string>();
-
-    const harFileJSONForStep = useMemo(() => {
-        if (harFileContents === undefined || selectedStep.endTimestamp === undefined || selectedStep.startTimestamp === undefined) {
-            return;
-        }
-
-        const stepStartTime = new Date(selectedStep.startTimestamp);
-        const stepEndTime = new Date(selectedStep.endTimestamp);
-
-        return harInTimeRange(harFileContents, stepStartTime, stepEndTime);
-
-    }, [harFileContents, selectedStep.endTimestamp, selectedStep.startTimestamp]);
-
-    useEffect(() => {
-        if (harFileJSONForStep === undefined) {
-            return;
-        }
-
-        const blob = new Blob([JSON.stringify(harFileJSONForStep, null, 2)], {type : 'application/json'});
-        const objectUrl = window.URL.createObjectURL(blob);
-        setHarFileObjectUrl(objectUrl);
-
-        return function cleanup() {
-            window.URL.revokeObjectURL(objectUrl);
-        }
-    }, [harFileJSONForStep])
 
     useLayoutEffect(() => {
 
@@ -77,15 +48,6 @@ export const StepResultTitlebar = function StepResultTitlebar({
                 <span className={styles.stepError}>{selectedStep.stepError?.message.substr(0, 50)}</span>
             </div>
             <div className={styles.tabs}>
-                {harFileObjectUrl && <div className={styles.downloadHar}>
-                    <a href={harFileObjectUrl} download="step-har-file.har">
-                    <div>
-                    <img src={downloadHarButton} alt="download har" />
-                        DOWNLOAD HAR FOR STEP
-                        <div className={styles.underline}></div>
-                    </div>
-                    </a>
-                </div>}
                 <div
                     className={classnames({ [styles.selected]: selectedTab === "screenshots" })}
                     onClick={() => { selectTab("screenshots"); }} >
