eb1838925aa61d522b24eb8dee58a60b102896b5
Bnaya Peretz <bnaya@testim.io>
2020-09-08 15:23:22 +0300

    Strip ansi where applicable

--- packages/client/src/components/StepResultTitlebar/StepResultTitlebar.tsx
+++ packages/client/src/components/StepResultTitlebar/StepResultTitlebar.tsx
@@ -4,6 +4,7 @@ import { useState, useRef, useLayoutEffect } from "react";
 import styles from './styles.module.css';
 import classnames from "classnames";
 import type { Har } from "har-format";
+import stripAnsi from "strip-ansi";
 
 export const StepResultTitlebar = function StepResultTitlebar({
     selectedStep,
@@ -21,6 +22,8 @@ export const StepResultTitlebar = function StepResultTitlebar({
     const [ hover, setHover ]  = useState<boolean>(false);
     const [ toolTipNeeded, setToolTipNeeded ]  = useState<boolean>(false);
     const stepNameRef = useRef<HTMLSpanElement>(null);
+    const stepErrorStr = selectedStep.stepError?.message ? stripAnsi(selectedStep.stepError?.message).substr(0, 100) : undefined;
+    const stepName = selectedStep.name ? stripAnsi(selectedStep.name) : undefined;
 
     useLayoutEffect(() => {
 
@@ -35,17 +38,16 @@ export const StepResultTitlebar = function StepResultTitlebar({
         return () => window.removeEventListener('resize', handleResize);
       }, [toolTipNeeded, selectedStep]);
 
-
     return (
         <div className={classnames(styles.stepTitleBar, {
             [styles.isClickimMode]: isClickimMode
         })}>
             <div className={styles.stepTitle}>
                 <span ref={stepNameRef} className={styles.stepName} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} >
-                 {selectedStep.name}
+                 {stepName}
                 </span>
                 { (hover && toolTipNeeded) && <div className={styles.tooltip} >{ selectedStep.name }</div> }
-                <span className={styles.stepError}>{selectedStep.stepError?.message.substr(0, 50)}</span>
+                <span className={styles.stepError}>{stepErrorStr}</span>
             </div>
             <div className={styles.tabs}>
                 <div
