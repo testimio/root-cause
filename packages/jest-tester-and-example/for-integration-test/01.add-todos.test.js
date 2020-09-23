2b34b7b237812f8d7de358c99491e54d9742be71
Bnaya Peretz <bnaya@testim.io>
2020-09-23 13:23:48 +0300

    stacktrace-related-improvments

---
+++ packages/jest-tester-and-example/for-integration-test/01.add-todos.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable quotes */
+/* eslint-disable prefer-arrow-callback */
+const { navigateToBaseUrl, sendSpecialCharacter } = require("./helpers");
+
+it("Should add 4 todos", async function () {
+    await navigateToBaseUrl(page);
+
+    await page.type(".new-todo", "Buy milk");
+    await sendSpecialCharacter(page, ".new-todo", "Enter");
+
+    await page.type(".new-todo", "Order Pizza");
+    await sendSpecialCharacter(page, ".new-todo", "Enter");
+
+    await page.type(".new-todo", "Wash the dishes");
+    await sendSpecialCharacter(page, ".new-todo", "Enter");
+
+    await page.type(".new-todo", "Talk to Ben");
+    await sendSpecialCharacter(page, ".new-todo", "Enter");
+
+    await expect(page).toMatchElement(".todo-list > :nth-child(1) label", {
+        text: "Buy milk",
+    });
+    await expect(page).toMatchElement(".todo-list > :nth-child(2) label", {
+        text: "Order Pizza",
+    });
+    await expect(page).toMatchElement(".todo-list > :nth-child(3) label", {
+        text: "Wash the dishes",
+    });
+    await expect(page).toMatchElement(".todo-list > :nth-child(4) label", {
+        text: "Talk to Ben",
+    });
+}, 10_000);
--- packages/jest-tester-and-example/jest.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    projects: [
-        '<rootDir>/for-integration-test/jest.config.js',
-    ],
-};
