028ad8ccf593e338c91a7588eb630559d526dfff
Bnaya Peretz <bnaya@testim.io>
2020-10-13 03:58:50 +0300

    wip

--- packages/jest-integration-test-projects/RootCauseJestEnvTranspile.js
+++ packages/jest-integration-test-projects/RootCauseJestEnvTranspile.js
@@ -1,16 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 // @ts-nocheck
 
 // Avoid registering ts-node twice
 if (!process[Symbol.for('ts-node.register.instance')]) {
-  // eslint-disable-next-line import/no-extraneous-dependencies
   const tsNode = require('ts-node');
 
   tsNode.register({
     transpileOnly: true,
-    // eslint-disable-next-line import/no-extraneous-dependencies
     compilerOptions: require('@testim/root-cause-jest/tsconfig').compilerOptions,
-    // skipIgnore: true,
-    // ignore: [],
   });
 }
 const Env = require('@testim/root-cause-jest/lib/RootCauseJestEnv');
