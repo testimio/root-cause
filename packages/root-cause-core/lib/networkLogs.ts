8a5f3c4d24160da3d41f1d0480f9e8105d06d149
Bnaya Peretz <bnaya@testim.io>
2020-10-14 12:37:23 +0300

    Use publihsed chrome har

--- packages/root-cause-core/lib/networkLogs.ts
+++ packages/root-cause-core/lib/networkLogs.ts
@@ -3,7 +3,7 @@ import type { Page as PlaywrightPage, CDPSession as PlaywrightCDPSession } from 
 import type { TestContext } from './TestContext';
 import type { BeforeAllHook, AfterAllHook } from './interfaces';
 import { isNotPlaywrightPage, isPlaywrightChromiumBrowserContext } from './utils';
-import chromeHar, { ChromeHarMessage } from 'chrome-har';
+import chromeHar, { ChromeHarMessage } from '@testim/chrome-har';
 import { addDisposer, runAllDisposers } from './hooksHandlersDisposersHelper';
 import fs from 'fs-extra';
 import path from 'path';
