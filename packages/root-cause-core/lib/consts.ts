674014725de2f5f07ddaf99e4e7b8de9c3519264
MrZordex <zordex@gmail.com>
2021-02-22 23:18:37 +0200

    Added an option to change consts.RESULTS_DIR_NAME

--- packages/root-cause-core/lib/consts.ts
+++ packages/root-cause-core/lib/consts.ts
@@ -1,6 +1,6 @@
 export const CONFIG_MODULE_NAME = 'root-cause';
-export const RESULTS_DIR_NAME = '.root-cause';
-export const RUNS_DIR_NAME = 'runs';
+export const RESULTS_DIR_NAME = process.env.ROOT_CAUSE_DIR_NAME ?? '.root-cause';
+export const RUNS_DIR_NAME = process.env.ROOT_CAUSE_DIR_NAME ? '' : 'runs';
 export const TEST_RESULTS_FILE_NAME = 'results.json';
 export const RUN_CONCLUSION_FILE_NAME = 'runConclusion.json';
 export const FALLBACK_RUN_ID = 'no_run_id';
